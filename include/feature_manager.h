506e08345f189213da917da63af7c70e2fd9b205
jiamen <2731275645@qq.com>
2020-04-03 22:10:23 +0800

    comments

--- include/feature_manager.h
+++ include/feature_manager.h
@@ -2,7 +2,127 @@
 // Created by zlc on 2020/1/18.
 //
 
-#ifndef JMVIO_FEATURE_MANAGER_H
-#define JMVIO_FEATURE_MANAGER_H
+#ifndef _JMVIO_FEATURE_MANAGER_H_
+#define _JMVIO_FEATURE_MANAGER_H_
+
+
+#include <list>
+#include <algorithm>
+#include <vector>
+#include <numeric>  // 此头文件是数值库的一部分
+#include <map>
+
+using namespace std;
+
+#include <eigen3/Eigen/Dense>
+using namespace Eigen;
+
+// #include <ros/console.h>
+// #include <ros/assert.h>
+
+#include "parameters.h"
+
+
+class FeaturePerFrame   // 每帧的基本数据, 每个路标点在一帧图像中的信息变量。
+{               // _point 每帧的特征点[x, y, z, u, v, vx, vy]  ,  cur_td IMU和cam同步时间差
+public:
+    double cur_td;
+    Vector3d point;     // 3D特征点坐标
+    Vector2d uv;
+    Vector2d velocity;
+
+    double z;           // 特征点的深度
+    bool is_used;       // 是否被用了
+    double parallax;    // 视差
+    MatrixXd A;         // 变换矩阵
+    VectorXd b;
+    double dep_gradient;
+
+    FeaturePerFrame(const Eigen::Matrix<double, 7, 1>& _point, double td)
+    {
+        point.x() = _point(0);
+        point.y() = _point(1);
+        point.z() = _point(2);
+
+        uv.x() = _point(3);
+        uv.y() = _point(4);
+
+        velocity.x() = _point(5);
+        velocity.y() = _point(6);
+
+        cur_td = td;
+    }
+};
+
+
+class FeaturePerId      // 某feature_id下的所有FeaturePerFrame。 每个路标点由多个连续的图像帧观测到
+{                       // 常用feature_id和观测第一帧start_frame、最后一帧endFrame
+public:
+    const int feature_id;       // 特征点ID索引
+    int start_frame;            // 首次被观测到时, 该帧的索引
+    vector<FeaturePerFrame> feature_per_frame;      // 能够观测到某个特征点的所有相关帧
+
+    int     used_num;           // 该特征点出现的次数
+    bool    is_outlier;         // 是否为外点
+    bool    is_margin;          // 是否Marg边缘化
+    double  estimated_depth;    // 估计的逆深度
+    int     solve_flag;         // 求解器： 0 haven't solve yet ; 1 solve succ; 2 solve fail;
+
+    Vector3d gt_p;
+
+    FeaturePerId(int _feature_id, int _start_frame)
+        : feature_id(_feature_id), start_frame(_start_frame),
+          used_num(0), estimated_depth(-1.0), solve_flag(0)
+    {
+
+    }
+
+    int endFrame();         // 返回最后一次观测到这个特征点的图像帧索引ID
+};
+
+
+class FeatureManager        // 管理所有特征点, 通过list容器存储特征点属性
+{
+private:
+    double compensatedParallax2(const FeaturePerId& it_per_id, int frame_count);
+    const Matrix3d* Rs;
+    Matrix3d ric[NUM_OF_CAM];
+
+public:
+    list<FeaturePerId> feature;     // 存储每一个特征点, 及其对应的每帧的属性
+    int last_track_num;
+
+    FeatureManager(Matrix3d _Rs[]);
+
+    void setRic(Matrix3d _ric[]);
+
+    void clearState();
+
+    int getFeatureCount();      // 窗口中被跟踪的特征点数量
+
+    // 特征点进入时检查视差,是否为关键帧
+    bool addFeatureCheckParallax(int frame_count, const map<int, vector<pair<int, Eigen::Matrix<double, 7, 1>>>>& image, double td);
+
+    void debugShow();
+
+    // 前后两帧之间匹配特征点3D坐标
+    vector<pair<Vector3d, Vector3d>> getCorresponding(int frame_count_l, int frame_count_r);
+
+    // void updateDepth(const VectorXd &x);
+
+    void setDepth(const VectorXd& x);       // 设置特征点逆深度
+    void removeFailures();
+    void clearDepth(const VectorXd& x);
+    VectorXd getDepthVector();
+    void triangulate(Vector3d Ps[], Vector3d tic[], Matrix3d ric[]);    // 特征点三角化求深度(SVD分解)
+    void removeBackShiftDepth(Eigen::Matrix3d marg_R, Eigen::Vector3d marg_P, Eigen::Matrix3d new_R, Eigen::Vector3d new_P);
+
+    void removeBack();                  // 边缘化最老帧, 直接将特征点保存的帧号前移
+    void removeFront(int frame_count);  // 边缘化次新帧，对特征点在次新帧的信息移除
+
+    void removeOutlier();       // 移除外点
+
+};
+
 
 #endif //JMVIO_FEATURE_MANAGER_H
--- src/feature_manager.cpp
+++ src/feature_manager.cpp
@@ -1,4 +1,119 @@
 //
 // Created by zlc on 2020/1/18.
 //
+#include "feature_manager.h"
 
+int FeaturePerId::endFrame() // 返回最后一次观测到这个特征点的图像帧索引ID
+{
+    return start_frame + feature_per_frame.size() - 1;
+}
+
+FeatureManager::FeatureManager(Matrix3d _Rs[]) : Rs(_Rs)
+{
+    for(int i=0; i < NUM_OF_CAM; i ++)
+    {
+        ric[i].setIdentity();
+    }
+}
+
+void FeatureManager::setRic(Matrix3d *_ric)
+{
+    for(int i=0; i<NUM_OF_CAM; i ++)
+    {
+        ric[i] = _ric[i];
+    }
+}
+
+void FeatureManager::clearState()
+{
+    feature.clear();
+}
+
+// 窗口被跟踪特征点的数量
+int FeatureManager::getFeatureCount()
+{
+    int cnt = 0;
+
+    for( auto &it : feature )   // 遍历特征点
+    {
+        it.used_num = it.feature_per_frame.size();  // 所有特征点被观测到的帧数
+
+        // 如果该特征点有两帧以上观测到了 且 第一次观测到帧数不是在最后
+        if( it.used_num >= 2 && it.start_frame < WINDOW_SIZE-2 )
+        {
+            cnt ++;   // 这个特征点是有效的
+        }
+    }
+
+    return cnt;
+}
+
+bool FeatureManager::addFeatureCheckParallax(int frame_count, const map<int, vector<pair<int, Eigen::Matrix<double, 7, 1> > > > &image, double td)
+{                                            // 窗口内帧的数目    // Feature Id     // Camera Id      // FeaturePerFrame: (x, y, z, u, v, vx, vy)
+    // ROS_DEBUG("input feature: %d", (int)image.size());   // 特征点数量
+    // ROS_DEBUG("num of feature: %d", getFeatureCount());  // 能够作为特征点的数量
+    double parallax_sum = 0;// 所有特征点视差总和
+    int parallax_num = 0;
+    last_track_num = 0;     // 被跟踪的个数
+
+    // 1.把image map中的所有特征点放入feature list容器中
+    // 遍历特征点, 看特征点是否在特征点的列表, 如果没在, 则将<FeatureID, Start_frame>存入到Feature列表中, 否则统计数目
+    for (auto& id_pts : image)
+    {
+        FeaturePerFrame f_per_fra(id_pts.second[0].second, td);
+
+        // 1.1 迭代器寻找feature list中是否有这feature_id
+        int feature_id = id_pts.first;  // i当前需要新加入的特征点ID
+        // 第3个参数是Lambda表达式
+        auto it = find_if(feature.begin(), feature.end(), [feature_id](const FeaturePerId &it)
+        {
+            return it.feature_id == feature_id;
+        });
+
+        // 1.2 如果没有则新建一个, 并在feature管理器的list容器最后添加: FeaturePerId、FeaturePerFrame、FeaturePerFrame
+        if ( it == feature.end() )
+        {
+            feature.push_back(FeaturePerId(feature_id, frame_count));
+            feature.back().feature_per_frame.push_back(f_per_fra);
+        }
+        // 1.3 之前有的话在FeaturePerFrame添加此特征点在此帧的位置和其他信息, 并统计数目.
+        else if ( it->feature_id == feature_id )
+        {
+            it->feature_per_frame.push_back(f_per_fra);
+            last_track_num ++;      // 此帧有多少相同特征点被跟踪
+        }
+    }
+
+    // 2. 追踪次数小于20 或者 窗口内帧的数目小于2, 是关键帧
+    if (frame_count <  2 || last_track_num < 20)
+    {
+        return true;
+    }
+
+    // 3. 计算每个特征在次新帧和次次新帧中的视差
+    for(auto& it_per_id : feature)
+    {
+        // 观测该特征点的： 起始帧小于倒数第三帧, 终止帧要大于倒数第二帧, 保证至少有两帧能观测到
+        if (it_per_id.start_frame <= frame_count - 2 &&
+            it_per_id.start_frame + int(it_per_id.feature_per_frame.size()) - 1 >= frame_count -1)
+        {
+            // 总视差: 该特征点在两帧的归一化平面上的坐标点的距离ans
+            parallax_sum += compensatedParallax2(it_per_id, frame_count);
+            parallax_num ++;
+        }
+    }
+
+    // 4.1 第一次加进去的, 是关键帧
+    if (parallax_num == 0)
+    {
+        return true;
+    }
+    else
+    {
+        // ROS_DEBUG("parallax_sum: %lf, parallax_num: %d", parallax_sum, parallax_num);
+        // ROS_DEBUG("current parallax: %lf", parallax_sum / parallax_num * FOCAL_LENGTH);
+
+        // 4.2 平均视差大于阈值的是关键帧
+        return parallax_sum / parallax_num >= MIN_PARALLAX;
+    }
+}
